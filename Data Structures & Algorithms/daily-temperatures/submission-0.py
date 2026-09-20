3adf4809bd21ce977b93f18186d4892895512486
Anjoelo Calderon <73631126+jelo-ca@users.noreply.github.com>
2026-09-20 14:17:36 -0700

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.py
@@ -0,0 +1,13 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > temperatures[stack[-1]]:
+                temp = stack.pop()
+                res[temp] = i - temp
+
+            stack.append(i)
+
+        return res
